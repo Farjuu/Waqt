9f7fb4417a08690d52c1c5adc76a669174d087c9
Farjana <farjanaakter431@gmail.com>
2021-08-27 20:52:21 +0600

    Location service enabled

--- app/src/main/java/dev/farjana/waqt/MainActivity.java
+++ app/src/main/java/dev/farjana/waqt/MainActivity.java
@@ -1,14 +1,142 @@
 package dev.farjana.waqt;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    TextView loc_country,locationAddress;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        grantPermission();
+        initialize();
+
+        LocationIsEnabledOrNot();
+
+        getcurrentlocation();
+
+
     }
+
+    private void getcurrentlocation() {
+
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,500,5,(LocationListener)this);
+         }catch (SecurityException s){
+            s.printStackTrace();
+        }
+    }
+
+    private void LocationIsEnabledOrNot() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsEnabled = false;
+        boolean networkEnabled = false;
+        try {
+            gpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            networkEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        if(!gpsEnabled &&  !networkEnabled){
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Enable Location Service")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", (dialogInterface, i) -> startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))).setNegativeButton("Cancel",null)
+                    .show();
+
+        }
+
+    }
+
+    private void grantPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(getApplicationContext(),
+
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION},100);
+
+        }
+
+    }
+    @Override
+    public void onLocationChanged( Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            loc_country.setText(addresses.get(0).getLocality());
+            loc_country.append(", ");
+            loc_country.append(addresses.get(0).getCountryName());
+          //  locationAddress.setText(addresses.get(0).getAddressLine(0));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    private void initialize() {
+        loc_country = findViewById(R.id.loc_txt);
+        locationAddress  =findViewById(R.id.loc_add);
+
+    }
+
+
+
 }
